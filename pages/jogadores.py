0bf7829bc05f90bae5ea80513103f01d6878c771
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-02-18 15:50:21 -0300

    Add principal.py line 8-10, contexto/ jogadores.py

--- pages/jogadores.py
+++ pages/jogadores.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
-st.set_page_config (layout="wide")
+df_data = st.session_state["data"]
+
+clubes = df_data["Club"].value_counts().index
+clube = st.sidebar.selectbox ("Clube", clubes)
