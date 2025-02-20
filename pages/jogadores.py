696f727b3bf96dc2c2164bdf1aeb59e79cc09717
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-02-19 23:12:49 -0300

    Alteração na linha 9, troca df_data por df_jogadores

--- pages/jogadores.py
+++ pages/jogadores.py
@@ -6,7 +6,7 @@ clubes = df_data["Club"].value_counts().index
 clube = st.sidebar.selectbox ("Clube", clubes)
 
 df_jogadores = df_data[df_data["Club"] == clube]
-jogadores = df_data["Name"].value_counts().index
+jogadores = df_jogadores["Name"].value_counts().index
 jogador = st.sidebar.selectbox ("Jogador", jogadores)
 
 estatistica_jogador = df_data[df_data["Name"] == jogador].iloc[0]
