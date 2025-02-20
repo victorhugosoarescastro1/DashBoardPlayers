102d281b6b7ec24fd7aec3432bb4c1677b1bfa5e
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-02-20 15:15:33 -0300

    Finalizando o Projetos

--- pages/times.py
+++ pages/times.py
@@ -10,3 +10,21 @@ df_data = st.session_state["data"]
 
 clubes = df_data["Club"].value_counts().index
 clube = st.sidebar.selectbox ("Clube", clubes)
+
+df_filtro = df_data[df_data["Club"] == clube].set_index("Name")
+
+st.image(df_filtro.iloc[0]["Club Logo"])
+st.markdown (f"## {clube}")
+
+columns = ["Age", "Photo", "Flag", "Overall", 'Value(£)', 'Wage(£)', 'Height(cm.)', 'Weight(lbs.)',
+           'Joined', 'Contract Valid Until', 'Release Clause(£)',]
+
+st.dataframe(df_filtro[columns],
+             column_config={
+                 "Overall": st.column_config.ProgressColumn("Overall", format='%d', min_value=0, max_value=100)
+                 "Value(£)": st.column_config.NumberColumn(),
+                 "Wage(£)": st.column_config.ProgressColumn("Weekly Wage", format="£%f", min_value=0, max_value=df_filtro["Wage(£)"].max()),
+                 "Photo": st.column_config.ImageColumn(),
+                 "Flag": st.column_config.ImageColumn("Country"),}, height=1000)
+
+df_data["Flag"]
